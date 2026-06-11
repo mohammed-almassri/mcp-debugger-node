7968c8ef334222d8902382eb7e26caffeb10336f
Mohammed Almassri <mohammed.m.almassri@gmail.com>
2026-06-11 12:46:14 -0500

    integration tests

---
+++ test/fixtures/function-call.js
@@ -0,0 +1,9 @@
+function double(value) {
+  const result = value * 2;
+  return result;
+}
+
+const input = 21;
+const output = double(input);
+
+setTimeout(() => {}, 60_000);
---
+++ test/fixtures/startup-exception.js
@@ -0,0 +1,1 @@
+throw new Error("fixture startup failure");
